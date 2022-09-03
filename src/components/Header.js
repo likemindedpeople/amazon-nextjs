c223dac65c0909f297356e12e09bfafc9ae95edf
drjw <danielrjaywagner@gmail.com>
2022-09-03 14:34:15 -0500

    finished search bar, started Right nav divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,10 @@ import {
 const Header = () => {
   return (
     <header>
-      {/* Top Nav */}
-      <div className='flex items-center flex-grow p-1 py-2 bg-amazon_blue'>
-        <div className='flex items-center flex-grow mt-2 sm:flex-grow-0'>
+      {/* Top nav */}
+      <div className='flex flex-grow p-1 py-2 secondary-center bg-amazon_blue'>
+        {/* Amazon image */}
+        <div className='flex flex-grow mt-2 secondary-center sm:flex-grow-0'>
           <Image
             className='cursor-pointer'
             src='https://links.papareact.com/f90'
@@ -21,7 +22,7 @@ const Header = () => {
           />
         </div>
         {/* Search bar */}
-        <div className='items-center flex-grow hidden h-10 bg-yellow-400 rounded-md cursor-pointer hover:bg-yellow-500 sm:flex'>
+        <div className='flex-grow hidden h-10 bg-yellow-400 rounded-md cursor-pointer secondary-center hover:bg-yellow-500 sm:flex'>
           <input
             className='flex-grow flex-shrink w-6 h-full p-2 px-4 rounded-l-md focus:outline-none'
             type='text'
@@ -29,9 +30,24 @@ const Header = () => {
 
           <SearchIcon className='h-12 p-4' />
         </div>
+        {/* Right */}
+        <div className='flex mx-6 space-x-6 text-xs text-white whitespace-nowrap secondary-center'>
+          <div className='cursor-pointer'>
+            <p>Hi Danny</p>
+            <p>Account & Lists</p>
+          </div>
+          <div className='cursor-pointer'>
+            <p>Returns</p>
+            <p>& Orders</p>
+          </div>
+          <div className='cursor-pointer'>
+            <ShoppingCartIcon className='h-10' />
+            <p>Basket</p>
+          </div>
+        </div>
       </div>
 
-      {/* Bottom Nav */}
+      {/* Bottom nav */}
       <div>bottom nav</div>
     </header>
   )
